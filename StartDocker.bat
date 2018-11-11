0e5cce302e8b43565b08fd2facd13e9ecd8366ad
Josh Quintus <josh.quintus@gmail.com>
2018-11-11 12:17:47 -0500

    Improved comments

--- StartDocker.bat
+++ StartDocker.bat
@@ -2,6 +2,15 @@
 REM ****************************************************************************
 REM Start a linux docker container configured to run 
 REM a git hub page website
+REM
+REM Docker file for reference
+REM https://github.com/Starefossen/docker-github-pages/blob/master/Dockerfile
+REM
+REM Specifying the full jekyll command to run is optional, but it
+REM allows for adding additional arguments.  Specifically, the `-I`
+REM argument which enables incremental site regeneration.  There are
+REM some drawbacks to this, but it can greatly speed up the
+REM generation time (~8 seconds instead of ~2 minutes)
 REM ****************************************************************************
 
 ECHO Starting gpages container
