2fea5f4002f1047bcae18664f4e938a4807d1f38
Josh Quintus <josh.quintus@gmail.com>
2018-11-11 12:14:38 -0500

    Update the start command to have faster generation times

--- StartDocker.bat
+++ StartDocker.bat
@@ -14,3 +14,5 @@ docker run -t --rm           ^
     -v "%CD%":/usr/src/app   ^
     -p 4000:4000             ^
     starefossen/github-pages
+    jekyll serve -d /_site --watch --force_polling -H 0.0.0.0 -P 4000 -I
+    
